d41e8aa439533a40839a4ba15b7bcfbe49f87a79
pkbhowmick <pkbhowmick007@gmail.com>
2020-10-05 13:59:47 +0600

    Update pipeline

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,5 +4,5 @@ import json
 def test_index(app, client):
     res = client.get('/hello')
     assert res.status_code == 200
-    expected = {'response': 'Hello!'}
+    expected = {'response': 'Hello'}
     assert expected == json.loads(res.get_data(as_text=True))
